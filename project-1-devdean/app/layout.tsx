8538859f5532926caadc6de0d47d6e89aff2e2c3
Anas <khananas49340@gmail.com>
2026-03-27 20:59:53 +0530

    Implement Posthog

--- project-1-devdean/app/layout.tsx
+++ project-1-devdean/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
+import { PostHogProvider } from "./providers";
+import { Suspense } from "react";
+import { PostHogPageView } from "./PostHogPageView";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -40,27 +43,32 @@ export default function RootLayout({
         "font-sans",
         geist.variable,
       )}>
-        <Navbar/>
-      <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-        <LightRays
-          raysOrigin="top-center-offset"
-          raysColor="#5dfeca"
-          raysSpeed={0.5}
-          lightSpread={0.9}
-          rayLength={1.4}
-          followMouse={true}
-          mouseInfluence={0.02}
-          noiseAmount={0}
-          distortion={0}
-          className="custom-rays"
-          pulsating={false}
-          fadeDistance={1}
-          saturation={1}
-        />
-      </div>
-      <main>
-        {children}
-      </main> 
+        <PostHogProvider>
+          <Suspense fallback={null}>
+            <PostHogPageView />
+          </Suspense>
+          <Navbar/>
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
+        <main>
+          {children}
+        </main>
+        </PostHogProvider>
       </body>
     </html>
   );
